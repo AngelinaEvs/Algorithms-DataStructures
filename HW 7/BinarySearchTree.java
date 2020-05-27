4ccdf8f8cff3926f3504869ba2840bd8c5fea65b
AngelinaEvs <57727402+AngelinaEvs@users.noreply.github.com>
2020-05-27 22:09:40 +0300

    BinarySearchTree

---
+++ HW 7/BinarySearchTree.java
@@ -0,0 +1,149 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+public class BinarySearchTree<T extends Comparable> {
+    private Node root;
+    private int size;
+
+    private class Node {
+        private Node parent;
+        private T data;
+        private Node left;
+        private Node right;
+
+        private Node(Node parent, T data) {
+            this.parent = parent;
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+        public Node getParent() {
+            return parent;
+        }
+
+        public void setParent(Node parent) {
+            this.parent = parent;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return (String) data;
+        }
+    }
+
+    public BinarySearchTree() {
+        root = null;
+        size = 0;
+    }
+
+    public void add(T data) {
+        if (data != null) {
+            if (root != null) {
+                add(root, data);
+            }
+            else {
+                root = new Node(null, data);
+                size++;
+            }
+        } else {
+            throw new NullPointerException("You  entered the null data");
+        }
+    }
+
+    private void add(Node node, T data) {
+        if (data.compareTo(node.getData()) > 0) {
+            if (node.getRight() == null) {
+                node.setRight(new Node(node, data));
+                size++;
+            } else add(node.getRight(), data);
+        } else {
+            if (node.getLeft() == null) {
+                node.setLeft(new Node(node,  data));
+                size++;
+            } else add(node.getLeft(), data);
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void DFS() {
+        System.out.print(root.getData().toString() + " ");
+        DFS(root);
+    }
+
+    private void DFS(Node node) {
+        if (node != null) {
+            DFS(node.getLeft());
+            System.out.print(node.getData().toString() + " ");
+            DFS(node.getRight());
+        }
+    }
+
+    public void BFS() {
+        Collection<Node> nodes = new ArrayList<>();
+        nodes.add(root);
+        childrenBFS(nodes);
+    }
+
+    private void childrenBFS(Collection<Node> nodes) {
+        Collection<Node> children = new ArrayList<>();
+        for (Node node : nodes) {
+            if (node != null) {
+                System.out.print(node.getData().toString() + " ");
+                if (node.getLeft() != null) children.add(node.getLeft());
+                if (node.getRight() != null) children.add(node.getRight());
+            }
+        }
+        if (children.size() > 0) childrenBFS(children);
+    }
+
+    @Override
+    public String toString() {
+        return "BinarySearchTree{" +
+                "root=" + root +
+                ", size=" + size +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BinarySearchTree<?> that = (BinarySearchTree<?>) o;
+        return size == that.size &&
+                Objects.equals(root, that.root);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(root, size);
+    }
+}
