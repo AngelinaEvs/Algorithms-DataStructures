ac49481ca3ad920cf79fb8b45239cd8be4ce8809
AngelinaEvs <57727402+AngelinaEvs@users.noreply.github.com>
2020-05-27 18:50:10 +0300

    Update MergeSort.java

--- HW 5/MergeSort.java
+++ HW 5/MergeSort.java
@@ -5,11 +5,15 @@ public class MergeSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] array = new int[sc.nextInt()];
+        int[] array2 = new int[array.length];
         for (int i = 0; i < array.length; i++) {
             array[i] = sc.nextInt();
+            array2[i] = array[i];
         }
         mergeSort(array, 0, array.length - 1);
-        System.out.println(Arrays.toString(array));
+        System.out.println("R: " + Arrays.toString(array));
+        mergeSortNoRecursive(array2, 0, array.length - 1);
+        System.out.println("NoR: " + Arrays.toString(array2));
     }
 
     private static void mergeSort(int[] array, int left, int right) {
@@ -35,5 +39,13 @@ public class MergeSort {
             }
         }
         if (right + 1 - left >= 0) System.arraycopy(mergeArray, left, array, left, right + 1 - left);
+    }
+
+    public static void mergeSortNoRecursive(int[] array, int left, int right) {
+        for (int i = 1; i < right - left; i = i * 2) {
+            for (int j = left; j < right - i; j = j + (2 * i)) {
+                merge(array, j, j + i, Integer.min(j + (2 * i), right));
+            }
+        }
     }
 }
